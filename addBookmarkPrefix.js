c5d4f561765ff434d35fbc356409404c021c6719
Yasir Erkam <yasir_erkam@outlook.com>
2019-08-17 18:09:21 +0300

    Update addBookmarkPrefix.js

--- addBookmarkPrefix.js
+++ addBookmarkPrefix.js
@@ -1,32 +1,23 @@
+function addBookmarkPrefix(bookmark, prefix) {
 
-function addBookmarkPrefix(bookmark) {
+    if (bookmark.children != null) {
 
-    var bookmarkPrefix = "";
-    if (bookmark.children[0] != null) {
-
+        var bookmarkPrefix = "";
         for (var i = 0; i < bookmark.children.length; i++) {
 
             if (bookmark.children.length >= 10 && i < 9)
-                bookmarkPrefix = "0" + (i + 1) + ".";
+                bookmarkPrefix = prefix + "0" + (i + 1) + ".";
             else
-                bookmarkPrefix = (i + 1) + ".";
+                bookmarkPrefix = prefix + (i + 1) + ".";
 
             bookmark.children[i].name = bookmarkPrefix.concat(" ", bookmark.children[i].name);
 
-            if (bookmark.children[i].children[0] != null) {
+            if (bookmark.children[i].children != null) {
 
-                for (var j = 0; j < bookmark.children[i].children.length; j++) {
-                    if (bookmark.children[i].children.length >= 10 && j < 9)
-                        bookmarkPrefix = (i + 1) + ".0" + (j + 1) + ".";
-                    else
-                        bookmarkPrefix = (i + 1) + "." + (j + 1) + ".";
-                    bookmark.children[i].children[j].name = bookmarkPrefix.concat(" ", bookmark.children[i].children[j].name);
-
-                }
-
+                addBookmarkPrefix(bookmark.children[i], bookmarkPrefix);
             }
 
         }
     }
 }
-addBookmarkPrefix(bookmarkRoot.children[0]);
+addBookmarkPrefix(bookmarkRoot, "");
